5406aba732a0505ac783ae037f1559cbf215fb22
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-12-22 15:39:22 +0100

    - fix processing so the store is closed after each call

--- src/main/kotlin/com/jalgoarena/data/ProblemsRepository.kt
+++ src/main/kotlin/com/jalgoarena/data/ProblemsRepository.kt
@@ -6,18 +6,33 @@ import jetbrains.exodus.entitystore.PersistentEntityStores
 
 class ProblemsRepository {
 
-    val problemsStore: PersistentEntityStore =
-            PersistentEntityStores.newInstance(Constants.problemsStorePath)
+    fun findAll(): List<Problem> {
+        val store = store()
 
-    fun findAll(): List<Problem> = problemsStore.computeInReadonlyTransaction {
-        it.getAll(Constants.problemEntityType).map { Problem.from(it) }
+        try {
+            return store.computeInReadonlyTransaction {
+                it.getAll(Constants.problemEntityType).map { Problem.from(it) }
+            }
+        } finally {
+            store.close()
+        }
     }
 
-    fun find(id: String): Problem? = problemsStore.computeInReadonlyTransaction { txn ->
-        txn.find(
-                Constants.problemEntityType,
-                Constants.problemId,
-                id
-        ).map { Problem.from(it) }.firstOrNull()
+    fun find(id: String): Problem? {
+        val store = store()
+
+        try {
+            return store.computeInReadonlyTransaction { txn ->
+                txn.find(
+                        Constants.problemEntityType,
+                        Constants.problemId,
+                        id
+                ).map { Problem.from(it) }.firstOrNull()
+            }
+        } finally {
+            store.close()
+        }
     }
+
+    private fun store() = PersistentEntityStores.newInstance(Constants.problemsStorePath)
 }
