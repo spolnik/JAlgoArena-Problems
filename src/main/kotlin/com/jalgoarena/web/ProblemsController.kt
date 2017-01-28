a269321f23e87fa8576d101e0753d4e3ebf5bb60
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-28 19:30:33 +0100

    - remove CORS - CORS is supported through API Gateway

--- src/main/kotlin/com/jalgoarena/web/ProblemsController.kt
+++ src/main/kotlin/com/jalgoarena/web/ProblemsController.kt
@@ -2,13 +2,11 @@ package com.jalgoarena.web
 
 import com.jalgoarena.data.ProblemsRepository
 import com.jalgoarena.domain.Problem
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import javax.inject.Inject
 
-@CrossOrigin
 @RestController
 class ProblemsController(
         @Inject private val repository: ProblemsRepository
